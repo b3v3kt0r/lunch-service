05c9d1d704264406ac32f7ed83c6edaac3f0d9d6
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-09-25 20:54:07 +0300

    add models to admin panel

--- menus/admin.py
+++ menus/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from menus.models import (
+    Menu,
+    Restaurant,
+    Vote
+)
+
+admin.site.register(Restaurant)
+admin.site.register(Menu)
+admin.site.register(Vote)
