e4bf829ca375ad254a018c17749731885245086c
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-09-25 13:55:04 +0300

    create base models for menus, user

--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    date = models.DateField()
+    items = models.CharField()
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    date = models.DateField()
