0489667bd6feed70c76ef72f8977b886959c4e41
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-09-26 01:38:24 +0300

    update views in user

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAdminUser
 
 from user.serializers import UserSerializer
 
