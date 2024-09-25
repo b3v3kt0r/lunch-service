994eb88e6ce2d87ae649c9dc826838b3af8ca584
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-09-25 19:54:59 +0300

    create base serializers for menus

---
+++ menus/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from menus.models import Restaurant, Menu, Vote
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ["id", "name", "address"]
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ["id", "restaurant", "date", "items"]
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ["id", "user", "menu", "date"]
