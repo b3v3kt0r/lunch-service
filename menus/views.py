00fdf81b65ce7e8b6940d8b6fb802f01d0ee66fb
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-09-25 20:08:19 +0300

    add base views for menus

--- menus/views.py
+++ menus/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from menus.models import Restaurant, Menu, Vote
+from menus.serializers import RestaurantSerializer, MenuSerializer, VoteSerializer
+
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
