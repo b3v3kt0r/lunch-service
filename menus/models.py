59e3eb290e64501f35853afe7a2c652ef29b3a0b
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-09-25 21:35:15 +0300

    fix models

--- menus/models.py
+++ menus/models.py
@@ -6,11 +6,17 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Menu(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     date = models.DateField()
     items = models.TextField()
+
+    def __str__(self):
+        return f"Restaurant: {self.restaurant.name}. Menu: {self.items}"
 
 
 class Vote(models.Model):
